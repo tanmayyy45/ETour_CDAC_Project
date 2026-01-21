778e197b7d05f328cdd4384148c624004933770e
Aishwarya138 <aishwaryarsurve@gmail.com>
2026-01-21 18:09:19 +0530

    Fix CustomerService interface after tour merge

--- src/main/java/com/etour/app/service/CustomerService.java
+++ src/main/java/com/etour/app/service/CustomerService.java
@@ -1,5 +1,14 @@
 package com.etour.app.service;
 
-public interface CustomerService {
+import java.util.List;
 
+import com.etour.app.entity.CustomerMaster;
+
+public interface CustomerService {
+	CustomerMaster registerCustomer(CustomerMaster customer);
+    List<CustomerMaster> getAllCustomers();
+    CustomerMaster getCustomerById(Integer id);
+    CustomerMaster updateCustomer(Integer id, CustomerMaster customer);
+    void deleteCustomer(Integer id);
+    CustomerMaster loginCustomer(String email, String password);
 }
